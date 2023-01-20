7a6812a8ebe6d521230d99574326d1a3d3b31d6a
Caleb Darling <calebgdarling@gmail.com>
2023-01-19 19:26:47 -0600

    Create meal-maker.js

---
+++ meal-maker.js
@@ -0,0 +1,26 @@
+let menu = {
+  _meal: '',
+  _price: 0,
+  set meal(mealToCheck) {
+    if (typeof mealToCheck === 'string') {
+      return this._meal = mealToCheck;
+    }
+  },
+  set price(priceToCheck) {
+    if (typeof priceToCheck === 'number') {
+      return this._price = priceToCheck;
+    }
+  },
+  get todaysSpecial() {
+    if (this._meal && this._price) {
+      return `Today's Special is ${this._meal} for $${this._price}!`;
+    } else {
+      `Meal or price was not set correctly!`;
+    }
+  }
+}
+
+menu.meal = 'pizza';
+menu.price = 3.50;
+
+console.log(menu.todaysSpecial);
